14d04948a7d08eab840631263d22f08b0a1aadc2
CN-HYC <53418389+CN-HYC@users.noreply.github.com>
2021-04-29 10:40:27 +0800

    Update firstclass.sql

--- server/sql/firstclass.sql
+++ server/sql/firstclass.sql
@@ -1,7 +1,41 @@
-INSERT INTO `firstclass`(`cate_id`, `cate_name`) VALUES (1, '洗护');
-INSERT INTO `firstclass`(`cate_id`, `cate_name`) VALUES (2, '生鲜');
-INSERT INTO `firstclass`(`cate_id`, `cate_name`) VALUES (3, '食品');
-INSERT INTO `firstclass`(`cate_id`, `cate_name`) VALUES (4, '百货');
-INSERT INTO `firstclass`(`cate_id`, `cate_name`) VALUES (5, '手机');
-INSERT INTO `firstclass`(`cate_id`, `cate_name`) VALUES (6, '电脑');
-INSERT INTO `firstclass`(`cate_id`, `cate_name`) VALUES (7, '配件');
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Link
+ Source Server Type    : MySQL
+ Source Server Version : 50712
+ Source Host           : localhost:3306
+ Source Schema         : shopping_mall
+
+ Target Server Type    : MySQL
+ Target Server Version : 50712
+ File Encoding         : 65001
+
+ Date: 29/04/2021 10:36:41
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for firstclass
+-- ----------------------------
+DROP TABLE IF EXISTS `firstclass`;
+CREATE TABLE `firstclass`  (
+  `cate_id` int(11) NOT NULL,
+  `cate_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`cate_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of firstclass
+-- ----------------------------
+INSERT INTO `firstclass` VALUES (1, '洗护');
+INSERT INTO `firstclass` VALUES (2, '生鲜');
+INSERT INTO `firstclass` VALUES (3, '食品');
+INSERT INTO `firstclass` VALUES (4, '百货');
+INSERT INTO `firstclass` VALUES (5, '手机');
+INSERT INTO `firstclass` VALUES (6, '电脑');
+INSERT INTO `firstclass` VALUES (7, '配件');
+
+SET FOREIGN_KEY_CHECKS = 1;
